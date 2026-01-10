7d2b895dc2ad952488562a31f79287fbc3b8341b
jp106623 <jmontgomery5914@gmail.com>
2026-01-09 20:12:15 -0500

    Add 3rd HIGH issue (innerHTML XSS) for D grade

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -2,26 +2,38 @@
  * User Profile Component
  */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface Props {
   user: {
     name: string;
     bio: string;
     website: string;
+    customHtml?: string;
   };
 }
 
 export default function UserProfile({ user }: Props) {
+  const customRef = useRef<HTMLDivElement>(null);
+
+  // Another XSS: innerHTML assignment
+  useEffect(() => {
+    if (customRef.current && user.customHtml) {
+      customRef.current.innerHTML = user.customHtml;
+    }
+  }, [user.customHtml]);
+
   return (
     <div className="profile">
       <h1>{user.name}</h1>
 
-      {/* VULNERABILITY: XSS via dangerouslySetInnerHTML */}
+      {/* XSS via dangerouslySetInnerHTML */}
       <div
         className="bio"
         dangerouslySetInnerHTML={{ __html: user.bio }}
       />
+
+      <div ref={customRef} className="custom-content" />
 
       <a href={user.website}>Website</a>
     </div>
